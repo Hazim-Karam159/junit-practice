5c500ebb29056393120b62ea921ad92e560fff59
Hazim Karam <hazimkaram159@gmail.com>
2025-05-18 07:36:10 +0300

    tdd: implement logic for divisible by five to return 'Buzz'

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -10,7 +10,8 @@ public class FizzBuzz {
     public static String compute(int i) {
         if(i % 3 == 0)
             return "Fizz";
-        
+        else if (i % 5 == 0)
+            return "Buzz";
         return null;
     }
 }
